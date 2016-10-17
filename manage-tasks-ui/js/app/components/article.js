36a29222cc3dbaa72eeff8be9d3340c0fad07abf
Aliaksandr Hurchanka <Aliaksandr_Hurchanka@epam.com>
2016-10-17 23:26:36 +0300

    [P-13]: Article component.

---
+++ manage-tasks-ui/js/app/components/article.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Article = () => {
+    return (
+      <blockquote>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+          <footer>Someone famous in <cite title="Source Title">Source Title</cite></footer>
+      </blockquote>
+    );
+}
+
+export default Article;
--- manage-tasks-ui/js/app/pages/home-page/index.js
+++ manage-tasks-ui/js/app/pages/home-page/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from 'app/components/header';
+import Article from 'app/components/article';
 
 export default class HomePage extends Component {
     constructor() {
@@ -10,6 +11,7 @@ export default class HomePage extends Component {
             <div>
                 <Header />
                 <h3>Home page!</h3>
+                <Article />
             </div>
         );
     }
